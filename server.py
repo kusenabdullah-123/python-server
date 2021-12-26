60153d6c6231fe2a695ad13284a205a6d6195100
Kusen Abdullah <kusena322@gmail.com>
2021-12-26 15:14:56 +0700

    server response

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 import socket
-
+import json
 
 # Define socket host and port
 SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 8000
-
+SERVER_PORT = 8080
+database = [
+    {
+        'nama': 'mei',
+    }, {
+        'nama': 'mei pro',
+    }
+]
 # Create socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -13,16 +19,16 @@ server_socket.listen()
 print(f'Listening on port {SERVER_PORT}')
 
 while True:
-    # Wait for client connections
+
     client_connection, client_address = server_socket.accept()
-
-    # Get the client request
-    request = client_connection.recv(1024).decode()
-    print(request)
-
-    # Send HTTP response
-    response = 'HTTP/1.0 200 OK\n\nHello World'
-    client_connection.sendall(response.encode())
+    request = client_connection.recv(2048).decode()
+    req = request.split('\r\n')[0].split(' ')
+    if req[0] == 'GET':
+        if req[1] == '/users':
+            client_connection.send(f'{req[2]} 200 OK\r\n'.encode())
+            client_connection.send(
+                'Content-Type : application/json\r\n\r\n'.encode())
+            client_connection.send(json.dumps(database).encode())
     client_connection.close()
 
 # Close socket
